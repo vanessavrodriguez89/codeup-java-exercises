9cc86b5de8422ad234ed2ebb85bc2c0dbb09cd3a
Vanessa Rodriguez <vanessa.v.rodriguez89@gmail.com>
2020-08-20 16:37:32 -0500

    created grocery class

---
+++ src/Grocery.java
@@ -0,0 +1,9 @@
+import util.Input;
+
+public class Grocery {
+    public static void main(String[] args) {
+        Input scan = new Input();
+        System.out.println("Would you like to create a grocery list?");
+        String groceryItem = scan.getString();
+    }
+}
